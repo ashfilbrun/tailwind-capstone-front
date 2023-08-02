b6c9c6622cf6c727b910a9ca59166dcdd656e4fd
ashfilbrun <ashesclaire@gmail.com>
2023-08-02 07:08:38 -0700

    add coming soon

--- src/views/CreateSymptom.jsx
+++ src/views/CreateSymptom.jsx
@@ -21,18 +21,10 @@
 //   },
 // ]
 
-// export default function CreateSymptom() {
-//   return (
-//     <ul className="divide-y divide-gray-200">
-//       {people.map((person) => (
-//         <li key={person.email} className="py-4 flex">
-//           <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-//           <div className="ml-3">
-//             <p className="text-sm font-medium text-gray-900">{person.name}</p>
-//             <p className="text-sm text-gray-500">{person.email}</p>
-//           </div>
-//         </li>
-//       ))}
-//     </ul>
-//   )
-// }
+export default function CreateSymptom() {
+  return (
+    <ul className="divide-y divide-gray-200">
+     <h1>Coming Soon...</h1>
+    </ul>
+  )
+}
